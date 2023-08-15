51bc72e9c15404ddbfd6c42ffc782b812789e4bc
Dani <daniel.varlan20@gmail.com>
2023-08-15 13:39:12 +0200

    WIP Heatmap generation

--- src/api/heatmapHandler.js
+++ src/api/heatmapHandler.js
@@ -1,4 +1,6 @@
 import dateTools from "../tools/dateTools";
+import h337 from "heatmap.js-fix";
+import dataService from "./dataService";
 
 // TODO: Rename
 export default class heatmapProvider {
@@ -6,12 +8,20 @@ export default class heatmapProvider {
     constructor() {
         this.canvasHeight = 1800;
         this.canvasWidth = 2400;
+        this.mode = '';
+        this.data = [];
+        this.heatmapConfig = {
+            radius: 40,
+            maxOpacity: 0.6,
+            minOpacity: 0,
+            blur: 0.85
+        };
     }
 
     createHeatmapContainers() {
         let heatmapContainerWrapper = document.getElementById('heatmap-container-wrapper');
-        let mode = vcs.ui.store.getters['heatmap/getMode'];
-        let data = vcs.ui.store.getters['heatmap/getSensorData'];
+        this.data = vcs.ui.store.getters['heatmap/getSensorData'];
+        this.mode = vcs.ui.store.getters['heatmap/getMode'];
 
         if (!heatmapContainerWrapper) {
             heatmapContainerWrapper = document.createElement('div');
@@ -19,19 +29,19 @@ export default class heatmapProvider {
             document.body.appendChild(heatmapContainerWrapper);
         }
 
-        if (mode === 'day') {
+        if (this.mode === 'day') {
             // Name: Timestamp + Uhrzeit -> '2023-01-01 00:00:00'
             for (let hour = 0; hour < 24; hour++) {
                 const heatmapContainer = document.createElement("div");
-                heatmapContainer.setAttribute('id', dateTools.createLableFromDateAndHour(data[0].timestamp, hour));
+                heatmapContainer.setAttribute('id', dateTools.createLableFromDateAndHour(this.data[0].timestamp, hour));
                 heatmapContainer.setAttribute('style', `height: ${this.canvasHeight}px; width: ${this.canvasWidth}px; z-index: 1337; pointer-events: none; display: none;`);
                 heatmapContainerWrapper.appendChild(heatmapContainer);
             }
-        } else if (mode === 'default') {
+        } else if (this.mode === 'default') {
             // Name: Timestamp -> '2023-01-01'
-            for (let day = 0; day < data.length; day++) {
+            for (let day = 0; day < this.data.length; day++) {
                 const heatmapContainer = document.createElement("div");
-                heatmapContainer.setAttribute('id', data[day].timestamp);
+                heatmapContainer.setAttribute('id', this.data[day].timestamp);
                 heatmapContainer.setAttribute('style', `height: ${this.canvasHeight}px; width: ${this.canvasWidth}px; z-index: 1337; pointer-events: none; display: none;`);
                 heatmapContainerWrapper.appendChild(heatmapContainer);
             }
@@ -40,6 +50,26 @@ export default class heatmapProvider {
 
     createHeatmapsForContainers() {
         // Hier auch bei dem 'default' mode den Mittelwert für den Tag berechnen (andere Funktion)
+        // In mehrere Funktionen aufteilen
+        if (this.mode === 'day') {
+            for (let hour = 0; hour < 24; hour++) {
+                let heatmapCanvas = h337.create({
+                    container: document.getElementById(dateTools.createLableFromDateAndHour(this.data[0].timestamp, hour)),
+                    radius: this.heatmapConfig.radius,
+                    maxOpacity: this.heatmapConfig.maxOpacity,
+                    minOpacity: this.heatmapConfig.minOpacity,
+                    blur: this.heatmapConfig.blur
+                });
+                // TODO: Check if background value is enabled
+                heatmapCanvas.setData({
+                    min: vcs.ui.store.getters['heatmap/getMinValue'],
+                    max: vcs.ui.store.getters['heatmap/getMaxValue'],
+                    data: dataInstance.heatmapData[time].data //TODO: Convert data in format for heatmap.js
+                });
+            }
+        } else if (this.mode === 'default') {
+
+        }
     }
 
     changeToNextHeatmap() {
