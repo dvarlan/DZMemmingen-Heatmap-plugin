857c4f306bdd318e305dc4a4f52a3bc00737e862
Dani <daniel.varlan20@gmail.com>
2023-10-08 13:54:44 +0200

    Updated converter script

--- src/scripts/cleanAndConvertData.py
+++ src/scripts/cleanAndConvertData.py
@@ -5,27 +5,54 @@ import time
 start_time = time.time()
 print("Starte Konvertierung...")
 
-df = pd.read_csv('inputData.csv', skiprows=1)
-df.columns = ['Sensorname', 'Wert', 'Zeitstempel']
+raw_df = pd.read_csv('inputData.csv', skiprows=1)
+raw_df.columns = ['Sensorname', 'Wert', 'Zeitstempel']
 
-# Zeitstempel auf die Uhrzeit reduzieren (z.B. 00:20:56 -> 00:00:00) & "Duplikate" entfernen
-df['Zeitstempel'] = df['Zeitstempel'].str.split(':').str[0] + ":00:00"
-df.drop_duplicates(subset=['Zeitstempel', 'Sensorname'], keep='first', inplace=True)
-
-# Temperaturwerte auf eine Nachkommastelle runden
-df.Wert = df.Wert.round(decimals=2).astype(float)
+# Temperaturwerte auf eine Nachkommastelle runden & Datumsspalte in korrekten Typ konvertieren
+raw_df.Wert = raw_df.Wert.round(decimals=2).astype(float)
+raw_df['Zeitstempel'] = pd.to_datetime(raw_df['Zeitstempel'])
 
 # Koordinaten zu den jeweiligen Stationen hinzufügen
-
 sensor_mappings = {
-    'Schrannenplatz Messpunkt 1': {'lat': 47.982948, 'lon': 10.182558}, # Neuer Name: Schrannenplatz Osten
-    'Schrannenplatz Messpunkt 2': {'lat': 47.982792, 'lon': 10.181391}, # Neuer Name: Schrannenplatz Westen
-    'Schrannenplatz Messpunkt 3': {'lat': 47.98288, 'lon': 10.182168}, # Neuer Name: Schrannenplatz Mitte
-    'Weinmarkt Messpunkt 1': {'lat': 47.984707, 'lon': 10.180835}, # Neuer Name: Weinmarkt Mitte
-    'Weinmarkt Messpunkt 2': {'lat': 47.984859, 'lon': 10.182212}, # Neuer Name: Weinmarkt Westen
-    'Weinmarkt Messpunkt 3': {'lat': 47.984721, 'lon': 10.181372} # Neuer Name: Weinmarkt Osten
+    'Schrannenplatz Messpunkt 1': {'lat': 47.982948, 'lon': 10.182558},  # Neuer Name: Schrannenplatz Osten
+    'Schrannenplatz Messpunkt 2': {'lat': 47.982792, 'lon': 10.181391},  # Neuer Name: Schrannenplatz Westen
+    'Schrannenplatz Messpunkt 3': {'lat': 47.98288, 'lon': 10.182168},  # Neuer Name: Schrannenplatz Mitte
+    'Weinmarkt Messpunkt 1': {'lat': 47.984707, 'lon': 10.180835},  # Neuer Name: Weinmarkt Mitte
+    'Weinmarkt Messpunkt 2': {'lat': 47.984859, 'lon': 10.182212},  # Neuer Name: Weinmarkt Westen
+    'Weinmarkt Messpunkt 3': {'lat': 47.984721, 'lon': 10.181372}  # Neuer Name: Weinmarkt Osten
 }
 
+def create_label_for_row(row):
+    return f"{row['Tag']} {row['Stunde']}"
+
+def calculate_mean_per_hour(dataframe):
+    result_list = []
+
+    for sensor_point in sensor_mappings:
+        temp_df = dataframe.loc[dataframe['Sensorname'] == sensor_point].copy()
+
+        temp_df['Tag'] = temp_df['Zeitstempel'].dt.date
+        temp_df['Stunde'] = temp_df['Zeitstempel'].dt.hour
+
+        temp_df_hourly = temp_df.groupby(['Tag', 'Stunde'])['Wert'].mean().reset_index()
+        temp_df_hourly['Sensorname'] = sensor_point
+        temp_df_hourly['Wert'] = temp_df_hourly['Wert'].round(decimals=1)
+
+        temp_df_hourly['Stunde'] = temp_df_hourly['Stunde'].astype(str).str.zfill(2) + ':00:00'
+        temp_df_hourly['Zeitstempel'] = temp_df_hourly.apply(create_label_for_row, axis=1)
+        temp_df_hourly.drop(columns=['Tag', 'Stunde'], inplace=True)
+
+        result_list.append(temp_df_hourly.values.tolist())
+
+    result_df = pd.DataFrame(columns=['Wert', 'Sensorname', 'Zeitstempel'])
+
+    for item in result_list:
+        result_df = pd.concat([result_df, pd.DataFrame(item, columns=['Wert', 'Sensorname', 'Zeitstempel'])])
+
+    return result_df
+
+# Temperatur-Mittelwert pro Stunde berechnen
+df = calculate_mean_per_hour(raw_df)
 
 def get_lat_lon(sensor):
     return pd.Series([sensor_mappings[sensor]['lat'], sensor_mappings[sensor]['lon']])
@@ -34,9 +61,7 @@ def get_lat_lon(sensor):
 df[['Lat', 'Lon']] = df['Sensorname'].apply(get_lat_lon)
 
 # Konvertieren des Dataframes in eine JSON Datei mit passendem Format
-
 df['Zeitstempel'] = pd.to_datetime(df['Zeitstempel'])
-
 json_data = []
 start_date = pd.Timestamp('2023-01-01')
 end_date = pd.Timestamp('2023-07-31')
